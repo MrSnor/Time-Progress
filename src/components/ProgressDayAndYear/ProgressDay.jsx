bf094e8b1a69bc32829f42270d9cccf9e2209883
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2024-01-07 00:34:51 +0530

    refactor ProgressDay and ProgressYear
    call update functions only once instead of calling it every second for year progress, and move function definition

--- src/components/ProgressDayAndYear/ProgressDay.jsx
+++ src/components/ProgressDayAndYear/ProgressDay.jsx
@@ -5,28 +5,28 @@ const ProgressDay = () => {
   const [progress, setProgress] = useState(0);
   const [timeLeft, setTimeLeft] = useState("0 hours left");
 
+  const totalMillisecondsInDay = 24 * 60 * 60 * 1000;
+
+  const updateProgress = () => {
+    const now = new Date();
+    const millisecondsPassed =
+      now.getHours() * 60 * 60 * 1000 +
+      now.getMinutes() * 60 * 1000 +
+      now.getSeconds() * 1000 +
+      now.getMilliseconds();
+    const percentage = (millisecondsPassed / totalMillisecondsInDay) * 100;
+    setProgress(percentage);
+
+    // get hours left in current day
+
+    const timeLeftInMilliseconds = totalMillisecondsInDay - millisecondsPassed;
+    const minutesLeft = Math.floor(timeLeftInMilliseconds / 1000 / 60);
+    const hoursLeft = Math.floor(minutesLeft / 60);
+
+    setTimeLeft(hoursLeft + " hours left");
+  };
+
   useEffect(() => {
-    const updateProgress = () => {
-      const now = new Date();
-      const totalMillisecondsInDay = 24 * 60 * 60 * 1000;
-      const millisecondsPassed =
-        now.getHours() * 60 * 60 * 1000 +
-        now.getMinutes() * 60 * 1000 +
-        now.getSeconds() * 1000 +
-        now.getMilliseconds();
-      const percentage = (millisecondsPassed / totalMillisecondsInDay) * 100;
-      setProgress(percentage);
-
-      // get hours left in current day
-
-      const timeLeftInMilliseconds =
-        totalMillisecondsInDay - millisecondsPassed;
-      const minutesLeft = Math.floor(timeLeftInMilliseconds / 1000 / 60);
-      const hoursLeft = Math.floor(minutesLeft / 60);
-
-      setTimeLeft(hoursLeft + " hours left");
-    };
-
     const interval = setInterval(updateProgress, 1000);
 
     return () => {
--- src/components/ProgressDayAndYear/ProgressYear.jsx
+++ src/components/ProgressDayAndYear/ProgressYear.jsx
@@ -5,30 +5,26 @@ const ProgressYear = () => {
   const [progress, setProgress] = useState(0);
   const [timeLeft, setTimeLeft] = useState("0 days left");
 
+  const updateProgress = () => {
+    const now = new Date();
+    const startOfYear = new Date(now.getFullYear(), 0, 1);
+    const endOfYear = new Date(now.getFullYear() + 1, 0, 1);
+    const totalMillisecondsInYear = endOfYear.getTime() - startOfYear.getTime();
+    const millisecondsPassed = now.getTime() - startOfYear.getTime();
+    const percentage = (millisecondsPassed / totalMillisecondsInYear) * 100;
+    setProgress(percentage);
+
+    const timeLeftInMilliseconds = totalMillisecondsInYear - millisecondsPassed;
+    const daysLeft = Math.floor(timeLeftInMilliseconds / (1000 * 60 * 60 * 24));
+
+    setTimeLeft(daysLeft + " days left");
+  };
+
   useEffect(() => {
-    const updateProgress = () => {
-      const now = new Date();
-      const startOfYear = new Date(now.getFullYear(), 0, 1);
-      const endOfYear = new Date(now.getFullYear() + 1, 0, 1);
-      const totalMillisecondsInYear =
-        endOfYear.getTime() - startOfYear.getTime();
-      const millisecondsPassed = now.getTime() - startOfYear.getTime();
-      const percentage = (millisecondsPassed / totalMillisecondsInYear) * 100;
-      setProgress(percentage);
-
-      const timeLeftInMilliseconds =
-        totalMillisecondsInYear - millisecondsPassed;
-      const daysLeft = Math.floor(
-        timeLeftInMilliseconds / (1000 * 60 * 60 * 24),
-      );
-
-      setTimeLeft(daysLeft + " days left");
-    };
-
-    const interval = setInterval(updateProgress, 1000);
+    const interval = setTimeout(updateProgress, 1000);
 
     return () => {
-      clearInterval(interval);
+      clearTimeout(interval);
     };
   }, []);
 
